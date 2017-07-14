2e3d27b5bae3010ad92363a4d59933a9c424e8ff
NMMD <1064588030@qq.com>
2017-07-14 15:53:40 +0800

    edit

--- app/src/main/java/com/example/baixuanzs/view/MainActivity.java
+++ app/src/main/java/com/example/baixuanzs/view/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.baixuanzs.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hellp
+        TextView view = (TextView) findViewById(R.id.txt);
+        view.setText("changge");
+
+        Button bt = (Button) findViewById(R.id.button);
+
+        bt.setText("click");
+        bt.setBackground(getDrawable(R.mipmap.ic_launcher_round));
     }
 }
